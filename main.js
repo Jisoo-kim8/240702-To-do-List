2486b3fc79f94047a5f2acee8b813dc60a21466c
김지수 <kimjisoo@gimjisuui-MacBook-Air-3.local>
2024-07-04 10:44:10 +0900

    1.1ver

--- main.js
+++ main.js
@@ -1,36 +1,35 @@
-// 유저가 값을 입력한다
-// + 버튼을 클릭하면, 할 일이 추가된다.
-// delete 버튼을 클릭하면, 할 일이 삭제된다
+// 유저가 값을 입력한다.
+// + 버튼을 클라하면, 할 일이 추가된다.
+// delete 버튼을 누르면, 할 일이 삭제된다.
 // check 버튼을 누르면 할 일이 끝나면서 밑줄이 생긴다.
-// '진행중', '끝남' 탭을 누르면, 언더바가 이동한다.
-// '끝남'탭에는 끝난 아이템을 넣고, '진행중' 탭에는 진행중인 아이템을 넣는다
-// 전체탭을 누르면 다시 전체 아이템으로 돌아옴 
+// 'All','to-do','done' 탭들을 누르면, 언더바가 이동한다.
+// 'to-do'탭에는 진행중인 item만, 'done'탭에는 완료한 item 만 넣는다
+// 'All'탭을 누르면 다시 전체 item으로 돌아옴
+
+let task_input = document.getElementById("task_input");
+let add_button = document.getElementById("add_button");
+let task_list = [];
+add_button.addEventListener("click", addTask); // 작동 안 됐던 이유 : L10에서 getAnimations 함수 넣어놓음 (바보)
 
 
-
-let task_input = document.getElementById("task_input")
-let Add_button = document.getElementById("Add_button")
-let taskList = []
-Add_button.addEventListener("click",addTask)
-
-
-
-function addTask(){
-    let taskContent = task_input.value
-    taskList.push(task_input)
+function addTask() {
+    let taskContent = task_input.value ; // 여기가 이상한데... 배열에 왜 할당이 안되노
+    task_list.push(taskContent);
+    console.log(task_list)
 }
 
-function render(){
-    let resultHTML =  '';
-    for(let i=0 ; i < taskList.length; i++) {
-        resultHTML += `<div class="tasks_button">
-                    <div>${taskList[i]}</div> 
-                    <div>
-                        <button>check</button>
-                        <button>delete</button>
-                    </div>
-                </div>`;
+function render() {
+    let resultHTML = ''
+    for(let i=0; i<task_list.length; i++){
+        resultHTML += `<div class="task_list">
+                <div>${task_list[i]}</div> 
+                <div>
+                    <button>check</button>
+                    <button>delete</button>
+                </div>
+            </div>`
     }
 
-    document.getElementById("tasks_board").innerHTML = resultHTML ;
+    document.getElementById("task_board").innerHTML = resultHTML ;
+    
 }
